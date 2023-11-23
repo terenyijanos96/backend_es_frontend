90d1941c52244b3b5b0c996f573eae2ef753a954
terenyijanos96 <terenyi.janos96@gmail.com>
2023-11-23 17:50:17 +0100

    Migrációs fájl kitöltése

--- backend/database/migrations/2023_11_23_164409_create_poets_table.php
+++ backend/database/migrations/2023_11_23_164409_create_poets_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Poet;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,9 +13,46 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('poets', function (Blueprint $table) {
-            $table->id();
+            $table->id('poet_id');
+            $table->string('name');
+            $table->smallInteger('birth_year');
             $table->timestamps();
         });
+
+        Poet::create([
+            'name' => 'József Attila',
+            'birth_year' => 1905,
+        ]);
+
+        Poet::create([
+            'name' => 'Ady Endre',
+            'birth_year' => 1877,
+        ]);
+
+        Poet::create([
+            'name' => 'Petőfi Sándor',
+            'birth_year' => 1823,
+        ]);
+
+        Poet::create([
+            'name' => 'Arany János',
+            'birth_year' => 1817,
+        ]);
+
+        Poet::create([
+            'name' => 'Babits Mihály',
+            'birth_year' => 1883,
+        ]);
+
+        Poet::create([
+            'name' => 'Nemes Nagy Ágnes',
+            'birth_year' => 1922,
+        ]);
+
+        Poet::create([
+            'name' => 'Radnóti Miklós',
+            'birth_year' => 1909,
+        ]);
     }
 
     /**
