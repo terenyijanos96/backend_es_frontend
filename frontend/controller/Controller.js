2bb5d4c4a93d327d4a4ead7559dd4abae29f5d55
terenyijanos96 <terenyi.janos96@gmail.com>
2023-11-23 19:03:39 +0100

    kezdetleges Controller létrehozása

--- frontend/controller/Controller.js
+++ frontend/controller/Controller.js
@@ -1,3 +1,16 @@
+import DataService from "../model/DataService.js";
+import UrlapView from "../view/Urlap/UrlapView.js"
+import TablaView from "../view/Tablazat/TablaView.js"
+
 export default class Controller {
-    constructor(){}
+    constructor(){
+        this.dataService = new DataService
+
+        this.dataService.getData("poets", this.megjelenit)
+    }
+
+    megjelenit(){
+        new UrlapView()
+        new TablaView()
+    }
 }
