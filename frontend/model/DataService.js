d0bcbce495aa035d99b52b2f1820ab46f1c86fd5
terenyijanos96 <terenyi.janos96@gmail.com>
2023-11-23 18:38:46 +0100

    frontend sciptek kezdőállapot

---
+++ frontend/model/DataService.js
@@ -0,0 +1,22 @@
+export default class DataService {
+    constructor(){
+        axios.defaults.baseURL = "http://localhost:8000/api/poets/"
+    }
+
+    getData(){
+        
+    }
+    
+    postData(){
+
+    }
+    
+    putData(){
+
+    }
+    
+    deleteData(){
+
+    }
+    
+}
